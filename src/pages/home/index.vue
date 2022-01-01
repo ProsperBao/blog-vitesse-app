f0d48e6a10059478421169f552d13a8d8f0eaeb0
FuBaooo <baihe2233@gmail.com>
2022-01-02 01:10:35 +0800

    feat: test code and web site status icon

---
+++ src/pages/home/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const { t } = useI18n()
+
+const list = ref<{date: string; title: string}[]>([])
+const loading = ref<boolean>(false)
+loading.value = true
+fetch('https://api.oick.cn/lishi/api.php')
+  .then(res => res.text())
+  .then((res) => {
+    const content = JSON.parse(res.replace('\n', '')).result
+    list.value = content
+  })
+  .finally(() => {
+    loading.value = false
+  })
+</script>
+
+<template>
+  <div>
+    {{ t('list.title') }}
+    <div v-show="loading">
+      {{ t('status.loading') }}
+    </div>
+    <div v-for="item in list" :key="item.date">
+      {{ item.title }}
+    </div>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
