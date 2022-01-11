ce931c0520499e912e9555b1fcbacbdf6313c7bf
FuBaooo <baihe2233@gmail.com>
2022-01-11 11:11:09 +0800

    chore: update deps

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -4,18 +4,13 @@ declare interface Window {
 
 // with vite-plugin-md, markdowns can be treat as Vue components
 declare module '*.md' {
-  import type { ComponentOptions } from 'vue'
-  const component: ComponentOptions
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
   export default component
 }
 
-// declare module '*.vue' {
-//   import Vue from 'vue'
-//   export default Vue
-// }
-
 declare module '*.vue' {
-  import type { defineComponent } from 'vue'
-  const Component: ReturnType<typeof defineComponent>
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
   export default Component
 }
