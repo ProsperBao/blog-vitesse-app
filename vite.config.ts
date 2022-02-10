db906d81c9eed464aa3d9c4233dd779f6b3aa331
FuBaooo <baihe2233@gmail.com>
2022-02-10 17:12:30 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -78,7 +78,7 @@ export default defineConfig({
         // auto import icons
         // https://github.com/antfu/unplugin-icons
         IconsResolver({
-          componentPrefix: '',
+          prefix: false,
           // enabledCollections: ['carbon']
         }),
       ],
