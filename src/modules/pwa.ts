ccb32447d7408bd21a7574d02ca752873f74b011
FuBaooo <baihe2233@gmail.com>
2022-03-28 17:19:11 +0800

    fix: change page

--- src/modules/pwa.ts
+++ src/modules/pwa.ts
@@ -6,7 +6,7 @@ export const install: UserModule = ({ isClient, router }) => {
     return
 
   router.isReady().then(async() => {
-    // const { registerSW } = await import('virtual:pwa-register')
-    // registerSW({ immediate: true })
+    const { registerSW } = await import('virtual:pwa-register')
+    registerSW({ immediate: true })
   })
 }
